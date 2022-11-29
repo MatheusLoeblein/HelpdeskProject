e128931322e720ae33983af6fed9828e02bc2b78
Matheus Loeblein <matheus.eai@gmail.com>
2022-11-29 20:20:41 -0300

    .

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -11,11 +11,7 @@ urlpatterns = [
          views.category, name="tarefas-category"),
     path('tarefa/<int:id>/', views.tarefa, name="tarefa"),
     path('tarefa/addcomment/<int:id>', views.addcomment, name="addcomment"),
-<<<<<<< HEAD
-    path('status/', views.situacao, name="status"),
-    path('prioridade/', views.prioridade, name="prioridade"),
-=======
     path('status/', views.status, name="status"),
     path('finalizados/', views.finalizado, name="finalizado")
->>>>>>> 72527e843dae0d3875bfbe9aca44b61a8e051cf3
+
 ]
