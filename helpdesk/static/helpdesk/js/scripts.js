86e2092f9b12ff767d62d5ddcff0c41727fc7b5b
Matheus Loeblein <matheus.eai@gmail.com>
2022-12-20 17:37:19 -0300

    Logout ajusted

--- helpdesk/static/helpdesk/js/scripts.js
+++ helpdesk/static/helpdesk/js/scripts.js
@@ -43,14 +43,14 @@
     })();
 
     
-    (() => {
-      const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
-      const formLogout = document.querySelector('.form-logout');
+  (() => {
+    const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+    const formLogout = document.querySelector('.form-logout');
     
-      for (const link of authorsLogoutLinks) {
-        link.addEventListener('click', (e) => {
-          e.preventDefault();
-          formLogout.submit();
-        });
-      }
-    })();
+    for (const link of authorsLogoutLinks) {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        formLogout.submit();
+      });
+    }
+  })();
